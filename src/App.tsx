b25fa3fa6d581483594e4b64f446f1199d5c2114
Kyle Xie <kyle.xie@auspost.com.au>
2020-11-10 15:40:23 +1100

    remove dev logs

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,45 @@ const currentFrame = (index:number):string => (
   `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
 )
 
+const easeOutExpo = (x: number): number => {
+  return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+};
+
+const easeOutQuint = (x: number): number => {
+  return 1 - Math.pow(1 - x, 5);
+}
+
+const easeOutQuart = (x: number): number => {
+  return 1 - Math.pow(1 - x, 4);
+}
+
+const easeOutCubic = (x: number): number => {
+  return 1 - Math.pow(1 - x, 3);
+}
+
 const html = document.documentElement;
 const frameCount = 147;
-const animateDuration = 100; //millisecond
+const animateDuration = 450; //millisecond
 
 function App() {
   const canvasRef = useRef<any>(null);
   const imgRef = useRef<any>(new Image());
   const timeRef = useRef<Date>(new Date());
   const currentFrameRef = useRef<number>(1);
+  const startFrameRef = useRef<number>(1);
   const destinationFrameRef = useRef<number>(1);
+  const animationIsOn = useRef<Boolean>(false);
 
   const animate = (context:any) => {
     const now: Date = new Date();
     const timeElapsed = +now - +timeRef.current;
+    const timeProgress = timeElapsed/animateDuration;
     if (timeElapsed > animateDuration) {
+      animationIsOn.current = false;
       return;
     }
-    const currentFrame = Math.floor(timeElapsed / animateDuration * (destinationFrameRef.current - currentFrameRef.current) + currentFrameRef.current);
+    const currentFrame = Math.floor(easeOutQuart(timeProgress) * (destinationFrameRef.current - startFrameRef.current) + startFrameRef.current);
+    animationIsOn.current = true;
     updateImage(context, currentFrame);
     requestAnimationFrame(() => animate(context))
   }
@@ -32,10 +53,6 @@ function App() {
     currentFrameRef.current = index;
     imgRef.current.src = currentFrame(index);
     context.drawImage(imgRef.current, 0, 0);
-  }
-
-  const interpolate = (to:number) => {
-
   }
 
   useEffect(() => {
@@ -63,9 +80,12 @@ function App() {
             frameCount - 1,
             Math.floor(scrollFraction * frameCount)
         ));
-        timeRef.current = new Date();
         destinationFrameRef.current = frameIndex;
-        requestAnimationFrame(() => animate(context));
+        if (!animationIsOn.current) {
+          timeRef.current = new Date();
+          startFrameRef.current = currentFrameRef.current;
+          requestAnimationFrame(() => animate(context));
+        }
       })
     }
   }, [])
